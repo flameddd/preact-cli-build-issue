a1bb98f837dd91ef54ce440a25856b408ce289b4
Jacky Hu <flameddd@gmail.com>
2020-11-08 15:53:52 +0800

    reproduce build issue

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,13 @@
 import { h } from 'preact';
+import "@justinribeiro/lite-youtube";
 import style from './style.css';
 
 const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
+  <div class={style.home}>
+    <h1>Home</h1>
+    <p>This is the Home component.</p>
+    <lite-youtube videoid="guJLfqTFfIw"></lite-youtube>
+  </div>
 );
 
 export default Home;
